a1f1edb00692d79c85a1ef294951d05c607c653e
Guilherme Pereira Monte <6352573355@info.local>
2024-01-31 17:21:49 -0300

    atividade 31-01

---
+++ ViaCep/App.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'react-native';
+
+import { ContainerApp } from './style';
+import { Header } from './src/Components/Header';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+
+export default () => {
+  let [fontsLoaded] = useFonts({
+    
+    Roboto_500Medium,
+    Roboto_700Bold,
+    
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+  return (
+    <ContainerApp>
+      <StatusBar backgroundColor={'transparent'} translucent/>
+      {/* Header */}
+    <Header/>
+      {/* HOMESCREEN */}
+    </ContainerApp>
+  );
+}
+
+};
---
+++ ViaCep/src/Components/Header/style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.View`
+    background-color: #FECC2B;
+    height: 20%;
+    width: 100%;
+   
+    border-radius: 0px 0px 15px 15px;
+    elevation: 20;
+    shadowColor: '#000';
+    shadowOpacity:0.4;
+    shadowRadius:15;
+`
+export const HeaderContent = styled.SafeAreaView`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`
+export const TextHeader = styled.Text`
+    font-size: 24px;
+    color: #333E33;
+   
+    
+`
